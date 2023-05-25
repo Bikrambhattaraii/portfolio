b6a27fe8b38e24617c95dcb04ec15f8d823c4273
bikram bhattarai <bikrambhattarai296@gmail.com>
2023-05-25 21:07:51 +0545

    cvsection

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Button from './components/reusablebutton/button.jsx'
 import Home from './components/homepage/hero'
 import Header from "../src/components/header/header"
+import Information from './components/homepage/information';
 function App() {
   return (
     <div className="App">
     <Header />
     <Home/>
+    <Information/>
     </div>
   );
 }
---
+++ src/components/homepage/information.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./information.css";
+
+
+const Information = () => {
+  return (
+    <div className="main-container-information-section">
+      <div className="information-left-container">
+        <h1>Hello, I’m Freelancer & Web Developer</h1>
+        <p>
+          Hello! I’m Bikram Bhattarai. student of Nagarjuna College of IT.
+          Recently I'm learning React Js and have Few knowledge about Computer
+          Science. Im passionate about learning Information and Technology .
+          currently im Web Developer with little knowlegde of HTML CSS
+          JavaScript, C-programming ,C++, PHP, MySQL and Asp .net
+        </p>
+      <button className="information-button">Download CV</button>
+      </div>
+      <div className="information-right-container">
+        <img src="" alt="" />
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Information;
